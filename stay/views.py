30c87a28e9eb0fe4296bb913e31415654c8b2b20
habitance <positipman@gmail.com>
2019-07-16 15:57:40 +0900

    added personnel in stay models.py

--- stay/views.py
+++ stay/views.py
@@ -1,4 +1,4 @@
-
+from django.shortcuts import render, redirect
 from django.forms import modelformset_factory
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
